68fc2e5bfe96677dd2cdf6be58ee52a1f169d1ec
Michael1099 <michal.sulima1999@gmail.com>
2023-10-10 10:46:09 +0200

    Initial Commit

---
+++ lab3/lab3App.swift
@@ -0,0 +1,17 @@
+//
+//  lab3App.swift
+//  lab3
+//
+//  Created by Michael on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct lab3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
